0566d00588a2843738351380aba9dc78f365a9da
xeno178531 <kubekkakao6@gmail.com>
2026-03-27 22:27:37 +0100

    Projektwoanie slide baru ustawien

--- scripts/texture_progress_bar.gd
+++ scripts/texture_progress_bar.gd
@@ -1,10 +1,23 @@
+# Wykorzystano pomoc ChatGPT
+
 extends TextureProgressBar
 
 @onready var slider: TextureRect = %TextureRect
+var dragging: bool = false
+
 
 func _gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ActionButton"):
-		var mouseX = get_local_mouse_position().x
-		var percent = mouseX / size.x * 100
-		self.value = percent
-		slider.position.x = mouseX - slider.size.x / 2
+		dragging = event.is_pressed()
+		update_bar(event.position.x)
+	elif event is InputEventMouseButton and not event.is_pressed():
+		dragging = false
+	elif event is InputEventMouseMotion and dragging:
+		update_bar(event.position.x)
+
+
+func update_bar(mouse_position_x:float):
+	var percent = mouse_position_x / size.x * 100
+	self.value = percent
+	slider.position.x = clamp((mouse_position_x - slider.size.x / 2), 0, 650)
+	
