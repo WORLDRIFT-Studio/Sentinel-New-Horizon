0fb2ae4fe43866026f1a23981d20c01036bac39d
xeno178531 <kubekkakao6@gmail.com>
2026-04-02 22:05:50 +0200

    Naprawiono wszystkie błedy. Teraz napewno

--- scripts/settings/sound_settings.gd
+++
@@ -1,9 +0,0 @@
-extends AudioStreamPlayer
-
-func _ready() -> void:
-	GameEvents.music_val_changed.connect(_on_music_val_changed)
-	
-func _on_music_val_changed(bus:String, value:float):
-	if bus == "Music":
-		self.
-AudioServer.
