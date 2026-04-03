95d795f61941375eebf4ee9d756208810e063d06
Empolic <kolak.maksymilian2010@gmail.com>
2026-04-03 20:26:33 +0200

    Autorzy new menu

--- scripts/main_menu_autorzy.gd
+++ scripts/main_menu_autorzy.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var btn_wyjscie:      Button = %Wyjscie
+@onready var btn_next:      Button = %Next
 
 func _ready() -> void:
 	if btn_wyjscie == null:
@@ -9,5 +10,14 @@ func _ready() -> void:
 		
 	btn_wyjscie.pressed.connect(_on_wyjscie_pressed)
 	
+	if btn_next == null:
+		push_error("Przycisk_Next not found! Check unique name.")
+		return  
+		
+	btn_next.pressed.connect(_on_next_pressed)
+	
 func _on_wyjscie_pressed() -> void:
 	TransitionScene.fade_to_scene("res://scenes/menu/main_menu.tscn")
+
+func _on_next_pressed() -> void:
+	TransitionScene.fade_to_scene("res://scenes/menu/main_menu_autorzy")
--- scripts/menu/main_menu_autorzy_side.gd
+++ scripts/menu/main_menu_autorzy_side.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var btn_wyjscie:      Button = %Wyjscie
-@onready var btn_next:      Button = %Next
+@onready var btn_back:      Button = %Back
 
 func _ready() -> void:
 	if btn_wyjscie == null:
@@ -10,14 +10,14 @@ func _ready() -> void:
 		
 	btn_wyjscie.pressed.connect(_on_wyjscie_pressed)
 	
-	if btn_next == null:
-		push_error("Przycisk_Next not found! Check unique name.")
+	if btn_back == null:
+		push_error("Przycisk_Back not found! Check unique name.")
 		return  
 		
-	btn_next.pressed.connect(_on_next_pressed)
+	btn_back.pressed.connect(_on_back_pressed)
 	
 func _on_wyjscie_pressed() -> void:
 	TransitionScene.fade_to_scene("res://scenes/menu/main_menu.tscn")
 
-func _on_next_pressed() -> void:
-	TransitionScene.fade_to_scene("res://scenes/menu/autorzy_side_menu.tscn")
+func _on_back_pressed() -> void:
+	TransitionScene.fade_to_scene("res://scenes/menu/main_menu_autorzy.tscn")
