101784661296495a0c579d91d8d853b9a617afad
xeno178531 <kubekkakao6@gmail.com>
2026-04-13 20:19:51 +0200

    Poprawiono wyszukiwarkę, przygotowano panel do listów gończych

--- scenes/search.gd
+++ scenes/search.gd
@@ -1,14 +1,13 @@
 extends LineEdit
 
+@export var no_data:Label
 @export var childrens_parent:Node
-
 
 func _on_text_changed(new_text: String) -> void:
 	new_text.to_lower()
 	
 	if new_text == "":
 		for child in childrens_parent.get_children():
-			var label = child.get_node("MarginContainer/HBoxContainer/Label")
 			child.visible = true
 	
 	else:
@@ -17,3 +16,13 @@ func _on_text_changed(new_text: String) -> void:
 			var option_text = label.text.to_lower()
 			
 			child.visible = option_text.contains(new_text)
+			
+	var any_visible = false
+	
+	for child in childrens_parent.get_children():
+		if child.visible:
+			any_visible = true
+
+	no_data.visible = !any_visible
+
+			
