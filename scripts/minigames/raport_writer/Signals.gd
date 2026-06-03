0faeeafe96b180ba50d0fc2b0b5da79776669494
Empolic <kolak.maksymilian2010@gmail.com>
2026-06-03 09:51:14 +0200

    usuwanie muzyki

--- scripts/minigames/raport_writer/Signals.gd
+++ scripts/minigames/raport_writer/Signals.gd
@@ -1,4 +1,5 @@
 extends Node
+
 @warning_ignore_start("unused_signal")
 signal IncrementScore(incr: int)
 signal IncrementCombo()
@@ -7,10 +8,11 @@ signal CreateFallingKey(button_name: String, hit_time: float)
 signal KeyListenerPress(button_name: String, array_num: int)
 
 var music_player: AudioStreamPlayer2D = null
+var manual_time: float = 0.0
 
 func get_song_time() -> float:
 	if music_player == null or not music_player.playing:
-		return 0.0
+		return manual_time
 	return music_player.get_playback_position() \
 		+ AudioServer.get_time_since_last_mix() \
 		- AudioServer.get_output_latency()
